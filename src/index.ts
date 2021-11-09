2a68a26308110f7b50a42ef92953a4733e9bd025
root <root@NB-JANSO.localdomain>
2021-11-09 12:45:19 +0100

    Deleted errors-throwing section.

--- src/index.ts
+++ src/index.ts
@@ -161,10 +161,6 @@ export = async function plugin(
 
   if (totalBrokenLinks) {
     const message = `${totalBrokenLinks} broken links found`;
-    if (process.env.NODE_ENV === 'production') {
-      // break builds with broken links before they get deployed for reals
-      throw new Error(message);
-    }
 
     if (verbose) {
       console.error(message);
